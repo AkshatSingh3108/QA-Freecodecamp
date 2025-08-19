2f7000fc8921a3c8d2db4146a250064b5278f151
Akshat Singh <130298155+AkshatSingh3108@users.noreply.github.com>
2025-08-19 10:30:54 +0530

    Add files via upload

---
+++ boilerplate-project-metricimpconverter/routes/api.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const expect = require('chai').expect;
+const ConvertHandler = require('../controllers/convertHandler.js');
+
+module.exports = function (app) {
+  
+  let convertHandler = new ConvertHandler();
+
+  app.get('/api/convert', (req, res) => {
+    const input = req.query.input;
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+
+    if (initNum === undefined && initUnit === undefined) {
+      return res.json('invalid number and unit');
+    }
+    if (initNum === undefined) {
+      return res.json('invalid number');
+    }
+    if (initUnit === undefined) {
+      return res.json('invalid unit');
+    }
+
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string
+    });
+  });
+
+};
